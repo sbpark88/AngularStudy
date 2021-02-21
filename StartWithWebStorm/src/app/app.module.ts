5ed75b9f114ba1b351de7d1739965ade14d25124
sbpark88 <saebyulpark88@gmail.com>
2021-02-21 18:01:42 +0900

    angular cli practice

--- StartWithWebStorm/src/app/app.module.ts
+++ StartWithWebStorm/src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { GreenDreamTreeComponent } from './green-dream-tree/green-dream-tree.component';
+import { HighlightDirective } from './highlight.directive';
+import {TodosModule} from './todos/todos.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    GreenDreamTreeComponent,
+    HighlightDirective,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TodosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
